bb22e278498fdd0da213b5beac9184e956ce8dca
efauser <efauser@ualberta.ca>
2012-11-29 19:09:03 -0700

    Added Analysis view to setup

--- PhotoWebApp/resources/sql/additionalSQL.sql
+++ PhotoWebApp/resources/sql/additionalSQL.sql
@@ -8,6 +8,7 @@ DROP INDEX subject_index;
 DROP INDEX place_index;
 DROP TABLE image_views;
 DROP SEQUENCE pic_id_sequence;
+DROP VIEW fact_table;
 
 CREATE SEQUENCE pic_id_sequence;
 
@@ -25,3 +26,12 @@ CREATE TABLE image_views (
     FOREIGN KEY(user_name) REFERENCES users,
     UNIQUE (photo_id, user_name)
 );
+
+CREATE VIEW fact_table AS SELECT owner_name, subject, timing as time,
+concat(concat(to_char(trunc(timing, 'DAY')), ' ~ '), to_char(trunc(timing, 'DAY')+6)) as week,
+concat(concat(to_char(trunc(timing, 'MM')), ' ~ '), to_char(last_day(timing))) as month,
+concat(concat(to_char(trunc(timing, 'YYYY')), ' ~ '), to_char(trunc(timing, 'YYYY')+364)) as year,
+((to_char(timing, 'W') * 7) + (to_char(timing, 'MM') * 30) + (to_char(timing, 'YY') * 365)) as week_sort,
+((to_char(timing, 'MM') * 30) + (to_char(timing, 'YY') * 365)) as month_sort,
+((to_char(timing, 'YY') * 365)) as year_sort from images;
+
